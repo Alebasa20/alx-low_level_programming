832265df73afd1c58028762c1f94475c3e42edf0
Alebasa20 <Alexandrebassa12@gmail.com>
2022-09-14 09:02:01 -0700

    task 5 is done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the number since 0 to 9
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+{
+	char n;
+
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(c);
+	}
+
+
+	putchar('\n');
+
+	return (0);
+
+}
