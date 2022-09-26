28bed00084b279aa31fa0a3c7408e048c52ef1d2
Alebasa20 <Alexandrebassa12@gmail.com>
2022-09-26 08:10:17 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * main - Print putchar
+ *
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	char c;
+	{
+		putchar('_');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,16 @@
+write a program that prints holberton, followed by a new line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
